d93cdb2e21729827a78d5eb36431f363cbcb358e
Richard Nguyen <richarbn@uci.edu>
2025-02-25 01:57:47 -0800

    -added funtion to write the collection table to a csv file called collection_data as (card_name, total_count)

--- db_operations.py
+++ db_operations.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 def connect_db(database = "adventuretimecardwars.db"):
     return sqlite3.connect(database)
@@ -30,10 +31,11 @@ def get_all_collection():
 def get_collection_card_count():
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute("""SELECT sets_cards.card_id, SUM(collection.quantity_owned * sets_cards.card_count) AS total_count
+    cursor.execute("""SELECT cards.card_name, SUM(collection.quantity_owned * sets_cards.card_count) AS total_count
                    FROM collection
                    JOIN sets_cards on collection.set_id = sets_cards.set_id
-                   GROUP BY sets_cards.card_id;
+                   JOIN cards on sets_cards.card_id = cards.card_id
+                   GROUP BY cards.card_name;
                    """)
     cards_in_collection = cursor.fetchall()
     conn.close()
@@ -69,7 +71,21 @@ def add_card_to_cards(card_name):
 
 def get_card_id(card_name):
     card_id = check_card_exist(card_name)
-    return card_id if card_id else None 
+    return card_id if card_id else None
+
+def get_card_name(card_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT card_name
+                   FROM cards
+                   WHERE card_id = ?""", (card_id,))
+    
+    result = cursor.fetchone()
+    conn.close()
+    if result:
+        return result[0]
+    else:
+        return None
 
 def delete_card_from_cards(card_id):
     conn = connect_db()
@@ -86,3 +102,21 @@ def get_sets(card_id):
     conn.commit()
     sets = cursor.fetchall()
     return sets
+
+def write_collection_to_csv():
+    collection_data = get_collection_card_count()
+
+    with open("collection_data.csv", mode = "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Card Name","Count"])
+        writer.writerows(collection_data)
+
+if __name__ == "__main__":
+
+    update_set_count_in_collections(1,0)
+    update_set_count_in_collections(2,0)
+    print(get_all_collection())
+
+    write_collection_to_csv()
+
+    
